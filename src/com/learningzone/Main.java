c7dca820ce7acc2262b5732b1eceeb37301507d8
Mukhtar <mukhtartanimugarba@gmail.com>
2020-11-21 16:20:03 +0100

    File update

--- src/com/learningzone/Main.java
+++ src/com/learningzone/Main.java
@@ -4,7 +4,9 @@
 
 //In this Java tutorial, we're going to download and install the necessary tools to build Java applications. So open up
 //your browser and search for JDK download. JDK is short for Java Development Kit, and it's basically software
-//development environment for building Java environments.
+//development environment for building Java applications. It has a compiler, it ahs a bunch of code that we could reuse,
+//it has a Java runtime environment and a bunch of other stuff. Head over to oracle.com to download Java SE which is
+//short for 'Java Standard Edition'.
 
 
 
