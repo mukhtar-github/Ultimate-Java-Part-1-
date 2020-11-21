f4a87ced52131fd9549566205eceda85645532c0
Mukhtar <mukhtartanimugarba@gmail.com>
2020-11-21 15:39:46 +0100

    File update

--- src/com/learningzone/Main.java
+++ src/com/learningzone/Main.java
@@ -3,7 +3,8 @@
 // 1- Setting Up the Development Environment
 
 //In this Java tutorial, we're going to download and install the necessary tools to build Java applications. So open up
-//your browser and search for
+//your browser and search for JDK download. JDK is short for Java Development Kit, and it's basically software
+//development environment for building Java environments.
 
 
 
