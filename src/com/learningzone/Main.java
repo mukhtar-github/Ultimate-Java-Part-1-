05b82fb96df21f513f2703ea43920b9e16ea10d5
Mukhtar <mukhtartanimugarba@gmail.com>
2020-11-21 15:21:03 +0100

    New Java Learning Project

---
+++ src/com/learningzone/Main.java
@@ -0,0 +1,18 @@
+// 1-GETTING STARTED
+
+// 1- Setting Up the Development Environment
+
+//In this Java tutorial, we're going to download and install the necessary tools to build Java applications. So open up
+//your browser and search for
+
+
+
+
+package com.learningzone;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
