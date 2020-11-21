54a0b55e33ba4ef665a71ca1a19028f606101e92
Mukhtar <mukhtartanimugarba@gmail.com>
2020-11-21 16:42:52 +0100

    File update

--- src/com/learningzone/Main.java
+++ src/com/learningzone/Main.java
@@ -6,7 +6,14 @@
 //your browser and search for JDK download. JDK is short for Java Development Kit, and it's basically software
 //development environment for building Java applications. It has a compiler, it ahs a bunch of code that we could reuse,
 //it has a Java runtime environment and a bunch of other stuff. Head over to oracle.com to download Java SE which is
-//short for 'Java Standard Edition'.
+//short for 'Java Standard Edition'. Now the next piece of software we need is a code editor. There are so many code
+//editors for building Java applications. The popular one are Netbeans, Eclipse and IntelliJ. In this Java course I'm
+//going to use IntelliJ
+
+
+// 2- Anatomy of a Java Program
+
+//
 
 
 
